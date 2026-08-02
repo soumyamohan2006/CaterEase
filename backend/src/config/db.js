275429742287b8b4e16260a82d28a0b99a4fd9bb
Shyam Mohan <shyam.mohan@techtaliya.com>
2026-08-02 08:21:05 +0530

    fix: add vercel.json and update CORS

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+    if (mongoose.connection.readyState === 1) return;
     try {
         await mongoose.connect(process.env.MONGODBURL);
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.log("Database connection failed:", error.message);
-        process.exit(1);
     }
 };
 
